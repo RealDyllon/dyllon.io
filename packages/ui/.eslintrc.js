88a9207d8dc0704bc9150aa7dec5931cadb186a1
dyllon <dyllon.g@outlook.com>
2024-11-05 12:46:46 +0800

    add ui pkg

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,8 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@repo/eslint-config/react.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
+  root: true,
+  extends: ['@repo/eslint-config/react-internal.js'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'no-redeclare': 'off',
   },
 };
